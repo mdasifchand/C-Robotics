90c30984fe4fab6890c9dc48a49d8247440c79f2
Asif <mdasifchand@gmail.com>
2021-02-09 00:00:00 +0100

    pushing files

--- include/MCMC/include/MCMC.h
+++ include/MCMC/include/MCMC.h
@@ -1,1 +1,26 @@
+#ifndef _MCMC_H
+#define _MCMC_H
 
+
+Template <class T>
+class MCMC{
+
+MCMC( std::vector<T>&& someVector )noexcept {
+    this._value(std::move(someVector));
+
+}
+
+constexpr int sampling(){
+ //TODO: sample a function for realizations
+
+}
+
+explicit MCMC(){}
+~MCMC(){}
+
+private:
+std::vector<T> _value;
+};
+
+
+#endif
